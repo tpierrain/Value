16a09a5e1f99777e532f1ddca7b036fd40e58111
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2017-11-18 00:28:47 +0200

    add EqualByValues<T> little class

---
+++ Value/EqualByValues.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Value.Shared
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Determines two collections are equal by values, i.e. ignoring the order of an elements,
+    /// but the exact elements contained.
+    /// </summary>
+    public class EqualByValues<T>  
+    {
+        private IEnumerable<T> _first;
+        private IEnumerable<T> _second;
+
+        public EqualByValues(IEnumerable<T> first, IEnumerable<T> second)
+        {
+            _first = first;
+            _second = second;
+        }
+
+        public bool Value()
+        {
+            bool ret = true;
+            HashSet<T> set = new HashSet<T>(_first);
+            foreach (var item in _second)
+            {
+                if (!set.Contains(item))
+                {
+                    ret = false;
+                    break;
+                }
+            }
+            return ret;
+        }
+
+        public static implicit operator bool(EqualByValues<T> equalByValues)
+        {
+            return equalByValues.Value();
+        }
+    }
+}
